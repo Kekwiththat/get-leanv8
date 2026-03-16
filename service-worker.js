a835b631e66aad4a9ef9794ea40945620d25c87d
Getleanfitness <94596584+Kekwiththat@users.noreply.github.com>
2026-03-16 00:43:06 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const cacheName = 'getlean-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/getlean-functions.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+    ))
+  );
+});
